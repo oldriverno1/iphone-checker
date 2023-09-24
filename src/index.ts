baa57667b0b5ee3670eef2721d432243eca1e3e0
Henry Wang <henrywangsime@gmail.com>
2023-09-24 15:35:28 +0800

    check availability every 1 minute

--- src/index.ts
+++ src/index.ts
@@ -8,11 +8,20 @@ const URL =
   'https://www.apple.com/tw/shop/fulfillment-messages?pl=true&mts.0=regular&mts.1=compact&parts.0=MU793ZP%2FA&searchNearby=true&store=R713';
 const EMAIL = process.env.RECEIVER || '';
 
-app
-  .checkAndNotify(URL, EMAIL)
-  .then(() => {
-    console.log('Check completed.');
-  })
-  .catch(error => {
-    console.error('An error occurred:', error);
-  });
+const checkAvailability = () => {
+  console.log('Checking availability...');
+  app
+    .checkAndNotify(URL, EMAIL)
+    .then(() => {
+      console.log('Check completed.');
+    })
+    .catch(error => {
+      console.error('An error occurred:', error);
+    });
+};
+
+// Call the function immediately to check right away
+checkAvailability();
+
+// Set an interval to check every 1 minute (60000 milliseconds)
+setInterval(checkAvailability, 60000);
